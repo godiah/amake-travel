9d155a0e00361e34638df563e8f23c57dc480d64
godiah <mosesgodiah@gmail.com>
2026-02-18 12:10:25 +0300

    fix: site name

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'AmakeTraveLite') }} | Your gateway to unforgetable adventures</title>
+    <title>AmakeTraveLite | Your gateway to unforgetable adventures</title>
     <meta name="description"
         content="Discover unforgettable experiences with AmakeTraveLite – from expert-led hikes and nature walks to immersive cultural tours across Kenya’s hidden gems. Start your adventure today.">
 
